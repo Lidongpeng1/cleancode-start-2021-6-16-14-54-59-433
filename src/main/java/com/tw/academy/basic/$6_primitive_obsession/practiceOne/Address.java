faf1a0b03f4f62cc88dbebc5be492f70df3500b1
ethan <liet@cargosmart.com>
2021-06-17 19:07:06 +0800

    在address类中生成getCity和getProvince方法

--- src/main/java/com/tw/academy/basic/$6_primitive_obsession/practiceOne/Address.java
+++ src/main/java/com/tw/academy/basic/$6_primitive_obsession/practiceOne/Address.java
@@ -6,4 +6,12 @@ public class Address {
     public Address(String address) {
         this.address = address;
     }
+
+    private String getCity() {
+        return this.address.substring(this.address.indexOf("省") + 1, this.address.indexOf("市"));
+    }
+
+    private String getProvince() {
+        return this.address.substring(0, this.address.indexOf("省"));
+    }
 }
