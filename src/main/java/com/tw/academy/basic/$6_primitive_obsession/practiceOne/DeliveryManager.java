336fac67332d12e6572124e6e96da454f5ea8258
ethan <liet@cargosmart.com>
2021-06-17 19:04:14 +0800

    创建Address类，在DeliveryManager中初始化赋值

--- src/main/java/com/tw/academy/basic/$6_primitive_obsession/practiceOne/DeliveryManager.java
+++ src/main/java/com/tw/academy/basic/$6_primitive_obsession/practiceOne/DeliveryManager.java
@@ -1,12 +1,18 @@
 package com.tw.academy.basic.$6_primitive_obsession.practiceOne;
 
 public class DeliveryManager {
+    private Address from;
+    private Address to;
+
     String toAddress;
     String fromAddress;
 
     public DeliveryManager(String fromAddress, String toAddress) {
         this.toAddress = toAddress;
         this.fromAddress = fromAddress;
+
+        this.from = new Address(fromAddress);
+        this.to = new Address(toAddress);
     }
 
     public DeliverCenter allocate(){
