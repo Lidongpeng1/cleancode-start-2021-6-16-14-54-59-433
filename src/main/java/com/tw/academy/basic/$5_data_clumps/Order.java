b58ea8e1e9e97895261a7683cbf752f0ff2870d1
ethan <liet@cargosmart.com>
2021-06-17 16:08:57 +0800

    revert

--- src/main/java/com/tw/academy/basic/$5_data_clumps/Order.java
+++ src/main/java/com/tw/academy/basic/$5_data_clumps/Order.java
@@ -3,17 +3,20 @@ package com.tw.academy.basic.$5_data_clumps;
 public class Order {
 
     private int orderNumber;
-    private Buyer buyer;
+    private String buyerName;
+    private String buyerPhoneNumber;
+    private String buyerAddress;
 
-    public Order(int orderNumber, Buyer buyer) {
+    public Order(int orderNumber, String buyerName, String buyerPhoneNumber, String buyerAddress) {
         this.orderNumber = orderNumber;
-
-        this.buyer = buyer;
-
+        this.buyerName = buyerName;
+        this.buyerPhoneNumber = buyerPhoneNumber;
+        this.buyerAddress = buyerAddress;
     }
 
     public String confirm(){
-        return buyer.confirm();
+        return String.format("Please confirm buyer information: buyer name is %s, " +
+                "buyer phone number is %s and buyer address is %s.", buyerName, buyerPhoneNumber, buyerAddress);
     }
 }
 
