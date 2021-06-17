d470aed599f37cffcff6bb59cf851953347c0669
ethan <liet@cargosmart.com>
2021-06-17 16:04:16 +0800

    refactor Order

--- src/main/java/com/tw/academy/basic/$5_data_clumps/Order.java
+++ src/main/java/com/tw/academy/basic/$5_data_clumps/Order.java
@@ -5,42 +5,11 @@ public class Order {
     private int orderNumber;
     private Buyer buyer;
 
-    public String getBuyerName() {
-        return buyer.getBuyerName();
-    }
-
-    public String getBuyerPhoneNumber() {
-        return buyer.getBuyerPhoneNumber();
-    }
-
-    public String getBuyerAddress() {
-        return buyer.getBuyerAddress();
-    }
-
-    private String buyerName;
-    private String buyerPhoneNumber;
-    private String buyerAddress;
-
-    public void setBuyerName(String buyerName) {
-        this.buyerName = buyerName;
-    }
-
-    public void setBuyerPhoneNumber(String buyerPhoneNumber) {
-        this.buyerPhoneNumber = buyerPhoneNumber;
-    }
-
-    public void setBuyerAddress(String buyerAddress) {
-        this.buyerAddress = buyerAddress;
-    }
-
-    public Order(int orderNumber, String buyerName, String buyerPhoneNumber, String buyerAddress) {
+    public Order(int orderNumber, Buyer buyer) {
         this.orderNumber = orderNumber;
 
-        this.buyer = new Buyer(buyerName,buyerPhoneNumber,buyerAddress);
+        this.buyer = buyer;
 
-        setBuyerName(buyer.getBuyerName());
-        setBuyerPhoneNumber(buyer.getBuyerPhoneNumber());
-        setBuyerAddress(buyer.getBuyerAddress());
     }
 
     public String confirm(){
